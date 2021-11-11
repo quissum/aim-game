5746d2b2812097b85189eacb0d8b6e24b9e99e62
quissum <kingvey13@gmail.com>
2021-11-11 22:29:13 +0200

    змінив розміри

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function finishGame() {
 
 function createRandomCircle() {
   const circle = document.createElement('div')
-  const size = getRandomNumber(10, 60)
+  const size = getRandomNumber(10, 30)
   const { width, height } = board.getBoundingClientRect()
   const x = getRandomNumber(0, width - size)
   const y = getRandomNumber(0, height - size)
